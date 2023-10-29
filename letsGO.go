0fb5d34890aa67c26909ccc4e4f7abe1763bd3a6
yssh-raj10 <yashraj.se10@gmail.com>
2023-10-29 21:41:19 +0530

    Final commit (mp)

--- letsGO.go
+++ letsGO.go
@@ -1,9 +1,18 @@
 package main
 
-func main(){
+func main() {
+	//cards := newDeck()
+
+	//cards.printIt()
+
+	//fmt.Print(cards.toString())
+
+	//cards.saveToFile("yooo")
+
+	//cards := newDeckFromFile("yooo")
+	//cards.printIt()
+
 	cards := newDeck()
-
+	cards.shuffle()
 	cards.printIt()
-
-
 }
--- type.go
+++ type.go
@@ -1,24 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 type deck []string
 
-func (d deck) printIt(){
-	for i, card := range d{
-		fmt.Println(i,card)
-	} 
+func (d deck) printIt() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
 }
 
-func newDeck() deck{
+func newDeck() deck {
 	cards := deck{}
-	suitCards := []string{"Spades", "Hearts", "Dimaonds","Clubs"}
+	suitCards := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
-	for _, suitCard := range suitCards{
-		for _, cardValue := range cardValues{
-			cards = append(cards,cardValue+"of"+suitCard )
-		}  
+	for _, suitCard := range suitCards {
+		for _, cardValue := range cardValues {
+			cards = append(cards, cardValue+" of "+suitCard)
+		}
 	}
 	return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Err:", err)
+		os.Exit(1)
+
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+
+	}
 }
